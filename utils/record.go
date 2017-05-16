eeb45ac059d38382b7237f21c5fcf268ef91792a
Valentin Kuznetsov <vkuznet@gmail.com>
2017-05-16 11:24:05 -0400

    Add String to Record

--- utils/record.go
+++ utils/record.go
@@ -1,7 +1,22 @@
 package utils
 
+import (
+	"encoding/json"
+
+	log "github.com/sirupsen/logrus"
+)
+
 // Record data-type defines ReqMgr2 data record
 type Record map[string]interface{}
+
+// String function implements Stringer interface
+func (r Record) String() string {
+	rec, err := json.Marshal(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(rec)
+}
 
 // Convert2Record converts given interface to Record data type
 func Convert2Record(item interface{}) Record {
