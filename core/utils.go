fa06e41bdd0ae439bc3bd62a07e92e6107d94589
Valentin Kuznetsov <vkuznet@gmail.com>
2017-05-19 13:08:12 -0400

    Create database if it does not exists

--- core/utils.go
+++ core/utils.go
@@ -6,6 +6,7 @@ package core
 import (
 	"fmt"
 	"net/url"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 
@@ -54,22 +55,49 @@ func InitCouch(couchUrl, dbName string) {
 	// open up Catalog DB
 	u, err := url.Parse(couchUrl)
 	if err != nil {
-		panic(err)
+		log.WithFields(log.Fields{
+			"couchUrl": couchUrl,
+			"step":     "urlParse",
+		}).Panic(err)
 	}
 
 	// create a new client
 	client, err := couchdb.NewClient(u)
 	if err != nil {
-		panic(err)
+		log.WithFields(log.Fields{
+			"couchUrl": couchUrl,
+			"url":      u,
+			"step":     "couchdb.NewClient",
+		}).Panic(err)
 	}
 	Client = client
 
 	// get some information about your CouchDB
 	info, err := client.Info()
 	if err != nil {
-		panic(err)
+		log.WithFields(log.Fields{
+			"couchUrl": couchUrl,
+			"url":      u,
+			"client":   client,
+			"step":     "client.Info()",
+		}).Panic(err)
 	}
 	log.Println(info)
+
+	// create a database if it does not exists
+	if _, err = client.Create(dbName); err != nil {
+		if strings.Contains(err.Error(), "exists") {
+			log.Warn(err)
+		} else {
+			log.WithFields(log.Fields{
+				"couchUrl": couchUrl,
+				"url":      u,
+				"client":   client,
+				"dbname":   dbName,
+				"step":     "client.Create(dbName)",
+			}).Panic(err)
+		}
+	}
 
 	DB = client.Use(dbName)
 }
